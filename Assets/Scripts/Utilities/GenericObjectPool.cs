ac85a2529807469e2b45241ae620ec3da725f72a
Vastav <vastavpansuriya4444@gmail.com>
2024-03-01 13:32:33 +0530

    Implement GetItem() method in Generic Object Pool

---
+++ Assets/Scripts/Utilities/GenericObjectPool.cs
@@ -0,0 +1,49 @@
+using CosmicCuration.Bullets;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Pool;
+using static CosmicCuration.Bullets.BulletPool;
+
+
+namespace CosmicCuration.Utilities
+{
+    public class GenericObjectPool<T> where T : class
+    {
+        private class PooledItem<T>
+        {
+            public T item;
+            public bool isUsed;
+        }
+
+        private List<PooledItem<T>> pooledObjects = new List<PooledItem<T>>();
+
+
+        protected T GetItem()
+        {
+            if (pooledObjects.Count > 0)
+            {
+                PooledItem<T> item = pooledObjects.Find(item => !item.isUsed);
+                if (item != null)
+                {
+                    item.isUsed = true;
+                    return item.item;
+                }
+            }
+            return CreateNewPooledObj();
+        }
+        private T CreateNewPooledObj()
+        {
+            PooledItem<T> newObj = new PooledItem<T>();
+            newObj.item = CreateObj();
+            newObj.isUsed = true;
+            pooledObjects.Add(newObj);
+            return newObj.item;
+        }
+
+        protected virtual T CreateObj()
+        {
+            throw new NotImplementedException("Child Class is not implemented this method");
+        }
+    }
+}
