3708a8e9f6493a5221b5fa5398dd3c2c9c85de3d
Vastav <vastavpansuriya4444@gmail.com>
2024-03-01 16:35:22 +0530

    Sub-Types-In-Object-Pool-Setup

---
+++ Assets/Scripts/PowerUps/PowerUpPool.cs
@@ -0,0 +1,38 @@
+﻿using CosmicCuration.Utilities;
+using UnityEngine;
+
+namespace CosmicCuration.PowerUps
+{
+    public class PowerUpPool : GenericObjectPool<PowerUpController>
+    {
+
+        private PowerUpData powerUpData;
+
+        public PowerUpController GetPowerUpController<T>(PowerUpData powerUpData) where T : PowerUpController
+        {
+            this.powerUpData = powerUpData;
+            return GetObj<T>();
+        }
+
+        protected override PowerUpController CreateObj<U>()
+        {
+            if (typeof(U) == typeof(Shield))
+            {
+                return new Shield(powerUpData);
+            }
+            else if (typeof(U) == typeof(RapidFire))
+            {
+                return new RapidFire(powerUpData);
+            }
+            else if (typeof(U) == typeof(DoubleTurret))
+            {
+                return new DoubleTurret(powerUpData);
+            }
+            else
+            {
+                Debug.LogError("Returning null");   
+                return null;
+            }
+        }
+    }
+}
