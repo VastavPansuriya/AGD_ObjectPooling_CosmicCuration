17de4f69e9b54872a8d32cfb1f952783cfdcf9ed
Vastav <vastavpansuriya4444@gmail.com>
2024-03-01 10:24:57 +0530

    Create a Bullet Pool

---
+++ Assets/Scripts/Bullets/BulletPool.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace CosmicCuration.Bullets
+{
+    public class BulletPool
+    {
+        public class PooledBullet
+        {
+            public BulletController Controller;
+            public bool isUsed;
+        }
+
+        private BulletView bulletView;
+        private BulletScriptableObject bulletScriptableObject;
+        private List<PooledBullet> pooledBullets = new List<PooledBullet>();
+
+        public BulletPool(BulletView bulletView, BulletScriptableObject bulletScriptableObject)
+        {
+            this.bulletView = bulletView;
+            this.bulletScriptableObject = bulletScriptableObject;
+        }
+    }
+}
+
